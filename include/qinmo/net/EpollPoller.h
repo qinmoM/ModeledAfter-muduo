e277dcdef1b186a1f4dc508e948c80e3e8062021
qinmoM_Linux <qinmo666@outlook.com>
2026-04-22 22:57:39 +0800

    feat(EpollPoller.h) : Complete implementation

--- include/qinmo/net/EpollPoller.h
+++ include/qinmo/net/EpollPoller.h
@@ -15,12 +15,23 @@ class EpollPoller : public Poller
 {
 public:
     EpollPoller(EventLoop* eventLoop);
+    ~EpollPoller();
+
+    EpollPoller(const EpollPoller&) = delete;
+    EpollPoller& operator()(const EpollPoller&) = delete;
 
 public:
     Timestamp poll(int timeoutMs, ChannelList& list) override;
 
     void setChannel(Channel& channel) override;
     void removeChannel(Channel& channel) override;
+
+private:
+    void epollCTL(int op, Channel& channel);
+
+private:
+    int epollfd_;
+    std::vector<epoll_event> eventsbuf_;
 
 };
 }
