5fb91a2749c733bd0d653d3d5cf7e4488c35a8d1
qinmoM_Linux <qinmo666@outlook.com>
2026-04-24 00:37:47 +0800

    feat(Channel.h) : Add comment and other function

--- include/qinmo/net/Channel.h
+++ include/qinmo/net/Channel.h
@@ -3,6 +3,8 @@
 #include "../base/Timestamp.h"
 #include "PollerConfig.h"
 #include <functional>
+#include <atomic>
+#include <memory>
 
 class EventLoop;
 
@@ -15,31 +17,62 @@ namespace net
 class Channel
 {
 public:
+    /// @brief event callback
     using EventCallback = std::function<void()>;
+    /// @brief event callback with timestamp
     using TimeEventCallback = std::function<void(qinmo::Timestamp)>;
 
+public:
+    Channel(EventLoop* eventLoop);
+
+    Channel(const Channel&) = delete;
+    Channel& operator()(const Channel&) = delete;
+
+public:
+    /// @brief get information of channel
     int fd() const;
     poller::EventsType events() const;
-    void set_revents(poller::EventsType revt);
+    bool isInLoop() const;
+    EventLoop* eventLoop() const;
 
+    /// @brief only used for returned events in Poller
+    void set_revents(poller::EventsType revt);
+    /// @brief remove current file descriptor events in EventLoop
+    void remove();
+
+    /// @brief set trigger event
+    /// @param ev lambda, function object or callback
     void setReadEvent(TimeEventCallback ev);
     void setWriteEvent(EventCallback ev);
     void setCloseEvent(EventCallback ev);
     void setErrorEvent(EventCallback ev);
 
+    /// @brief enable and disable event
     void enableRead();
     void disableRead();
     void enableWrite();
     void disableWrite();
     void disableAll();
 
+    /// @brief determine what events in current channel
+    /// @return true if event exists
     bool isNone() const;
     bool isRead() const;
     bool isWrite() const;
     bool isAll() const;
 
+    /// @brief trigger all revents
+    void handle(Timestamp timestamp);
+
+private:
+    /// @brief automatic update listening events in the EventLoop
+    void updateInEventLoop();
+
 private:
     EventLoop* loop_;
+    std::atomic<bool> isInLoop_{false};
+
+    // std::weak_ptr<void> tie_;
 
     int fd_;
     poller::EventsType events_;
