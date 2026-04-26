b737e3f02917aff356ad0b14c7f6f11966849ea9
qinmoM_Linux <qinmo666@outlook.com>
2026-04-26 18:51:56 +0800

    feat(EventLoop) : encupsulate poller and channel.

---
+++ include/qinmo/net/EventLoop.h
@@ -0,0 +1,70 @@
+#pragma once
+
+#include "qinmo/net/Poller.h"
+#include "qinmo/net/detail/Wrapper.h"
+#include "qinmo/base/ThreadConfig.h"
+#include <mutex>
+
+namespace qinmo
+{
+/// @namespace qinmo::net
+namespace net
+{
+
+class EventLoop
+{
+public:
+    using Functor = std::function<void()>;
+
+public:
+    EventLoop();
+    ~EventLoop();
+
+    EventLoop(const EventLoop&) = delete;
+    EventLoop& operator()(const EventLoop&) = delete;
+
+    EventLoop(EventLoop&&) = delete;
+    EventLoop& operator()(EventLoop&&) = delete;
+
+public:
+    void loop();
+    void quit();
+
+    void setTimeoutMs(int ms);
+
+    bool hasChannel(Channel& channel) const;
+    void setChannel(Channel& channel);
+    void removeChannel(Channel& channel);
+
+    void wakeup();
+    void wakeupRead();
+
+    void runInLoop(Functor func);
+    void queueInLoop(Functor func);
+
+    bool isInCurrentThread() const;
+
+private:
+    void pendingFunctors();
+
+private:
+    std::unique_ptr<Poller> poller_;
+    Timestamp pollerReturnTime_;
+    std::atomic<int> timeoutMs_;
+
+    int wakeupfd_;
+    std::unique_ptr<Channel> wakeupChannel_;
+
+    ChannelList activeChannels_;
+
+    std::vector<Functor> pendingFunctors_;
+    std::mutex mutex_;
+
+    std::atomic<bool> quit_;
+    std::atomic<bool> looping_;
+    std::atomic<bool> pending_;
+    const qinmo::detail::ThreadType tid_;
+
+};
+} // namespace net
+} // namespace qinmo
---
+++ src/EventLoop.cpp
@@ -0,0 +1,134 @@
+#include "qinmo/net/EventLoop.h"
+
+namespace qinmo
+{
+/// @namespace qinmo::net
+namespace net
+{
+
+EventLoop::EventLoop()
+    : poller_(Poller::create(this))
+    , pollerReturnTime_(0)
+    , timeoutMs_(2000)
+    , wakeupfd_(qinmo::detail::eventfd(0, EFD_NONBLOCK | EFD_CLOEXEC))
+    , wakeupChannel_(new Channel(this, wakeupfd_))
+    , tid_(qinmo::detail::getTid())
+    , quit_(false)
+    , looping_(false)
+    , pending_(false)
+{
+    wakeupChannel_->setReadEvent( [this](Timestamp) -> void { wakeupRead(); } );
+    wakeupChannel_->enableRead();
+}
+
+EventLoop::~EventLoop()
+{
+    wakeupChannel_->disableAll();
+    wakeupChannel_->remove();
+    qinmo::detail::close(wakeupfd_);
+}
+
+void EventLoop::setTimeoutMs(int ms)
+{
+    timeoutMs_.store(ms);
+}
+
+void EventLoop::loop()
+{
+    looping_.store(true);
+    quit_.store(false);
+
+    while (!quit_.load())
+    {
+        // clear processed events
+        activeChannels_.clear();
+        pollerReturnTime_ = poller_->poll(timeoutMs_.load(), activeChannels_);
+
+        for (Channel* channel : activeChannels_)
+            channel->handle(pollerReturnTime_);
+
+        pendingFunctors();
+    }
+
+    looping_.store(false);
+}
+
+void EventLoop::quit()
+{
+    quit_.store(true);
+
+    if (!isInCurrentThread())
+        wakeup();
+}
+
+void EventLoop::wakeup()
+{
+    uint64_t one = 1;
+    qinmo::detail::write(wakeupfd_, &one, sizeof(one));
+    // logger
+}
+
+void EventLoop::wakeupRead()
+{
+    uint64_t one = 1;
+    qinmo::detail::read(wakeupfd_, &one, sizeof(one));
+    // logger
+}
+
+bool EventLoop::hasChannel(Channel& channel) const
+{
+    return poller_->findChannel(channel);
+}
+
+void EventLoop::setChannel(Channel& channel)
+{
+    poller_->setChannel(channel);
+}
+
+void EventLoop::removeChannel(Channel& channel)
+{
+    poller_->removeChannel(channel);
+}
+
+void EventLoop::runInLoop(Functor func)
+{
+    if (isInCurrentThread())
+        func();
+    else
+        queueInLoop(std::move(func));
+}
+
+void EventLoop::queueInLoop(Functor func)
+{
+    {
+        std::lock_guard<std::mutex> lock(mutex_);
+        pendingFunctors_.push_back(std::move(func));
+    }
+
+    if (!isInCurrentThread() || pending_)
+        wakeup();
+}
+
+bool EventLoop::isInCurrentThread() const
+{
+    return tid_ == qinmo::detail::getTid();
+}
+
+void EventLoop::pendingFunctors()
+{
+    std::vector<Functor> functors;
+    pending_.store(true);
+
+    {
+        std::lock_guard<std::mutex> lock(mutex_);
+        functors.swap(pendingFunctors_);
+    }
+
+    for (const Functor& func : functors)
+        func();
+
+    pending_.store(false);
+}
+
+} // namespace net
+} // namespace qinmo
