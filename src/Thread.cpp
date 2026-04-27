31f5824b8e5f447044395f5f0976d3a5814e2d1e
qinmoM_Linux <qinmo666@outlook.com>
2026-04-27 22:19:06 +0800

    feat(Thread.cpp) : Implemention of thread class

---
+++ src/Thread.cpp
@@ -0,0 +1,68 @@
+#include "qinmo/base/Thread.h"
+
+namespace qinmo
+{
+
+Thread::Thread(ThreadFunc func)
+    : func_(std::move(func))
+    , started_(false)
+    , joined_(false)
+    , id_(qinmo::detail::threadIDTypeEmpty)
+{ }
+
+Thread::~Thread()
+{
+    if (started_ && !joined_.load())
+        std::terminate();
+}
+
+bool Thread::started() const
+{
+    return started_.load();
+}
+
+bool Thread::joined() const
+{
+    return joined_.load();
+}
+
+qinmo::detail::ThreadIDType Thread::getID() const
+{
+    return id_;
+}
+
+void Thread::start()
+{
+    if (started_.load())
+        return;
+
+    started_.store(true);
+    thread_ = std::thread(
+        [this]() -> void
+        {
+            qinmo::detail::ThreadIDType id = qinmo::detail::getTid();
+
+            {
+                std::unique_lock<std::mutex> lock(mutex_);
+                id_ = id;
+            }
+
+            cv_.notify_all();
+            func_();
+        }
+    );
+
+    std::unique_lock<std::mutex> lock(mutex_);
+    cv_.wait(lock, [this]() -> bool { return qinmo::detail::threadIDTypeEmpty != id_; });
+}
+
+void Thread::join()
+{
+    if (!started_.load() || joined_.load()) return;
+
+    joined_.store(true);
+    if (thread_.joinable())
+        thread_.join();
+}
+
+}
