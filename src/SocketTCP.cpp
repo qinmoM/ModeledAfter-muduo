909b686a40c5ec9c17f3f3c4576bbbca167141bd
qinmoM_Linux <qinmo666@outlook.com>
2026-05-15 18:43:45 +0800

    feat(SocketTcp.cpp) : implement

--- src/SocketTCP.cpp
+++ src/SocketTCP.cpp
@@ -4,9 +4,17 @@
 namespace qinmo::net
 {
 
-SocketTCP SocketTCP::create(const InetAddr& addr, int flags)
+SocketTCP SocketTCP::createRaw(const InetAddr& addr, int flags)
 {
+    if (!addr.isValid())
+        return SocketTCP();
+
     return SocketTCP(detail::socket((addr.isIPv4() ? AF_INET : AF_INET6), SOCK_STREAM | flags));
+}
+
+SocketTCP SocketTCP::createNonBlockOrDie(const InetAddr& addr)
+{
+    return createRaw(addr, SOCK_NONBLOCK | SOCK_CLOEXEC);
 }
 
 SocketTCP SocketTCP::attach(const int fd)
@@ -57,6 +65,7 @@ int SocketTCP::getfd() const
 InetAddr SocketTCP::getLocalAddr() const
 {
     detail::sockaddr addr;
+    detail::zeroMemory(&addr, sizeof(addr));
     if (!detail::getsockname(sockfd_, addr))
         detail::sockaddr_cast<detail::sockaddr, sockaddr>(&addr)->sa_family = AF_UNSPEC;
 
@@ -66,10 +75,21 @@ InetAddr SocketTCP::getLocalAddr() const
 InetAddr SocketTCP::getPeerAddr() const
 {
     detail::sockaddr addr;
+    detail::zeroMemory(&addr, sizeof(addr));
     if (!detail::getpeername(sockfd_, addr))
         detail::sockaddr_cast<detail::sockaddr, sockaddr>(&addr)->sa_family = AF_UNSPEC;
 
     return InetAddr(addr);
+}
+
+ssize_t  SocketTCP::recv(char* buf, size_t len)
+{
+    return detail::recv(sockfd_, buf, len);
+}
+
+ssize_t  SocketTCP::send(const char* buf, size_t len)
+{
+    return detail::send(sockfd_, buf, len);
 }
 
 bool SocketTCP::bind(const InetAddr& addr)
@@ -91,9 +111,18 @@ bool SocketTCP::listen(int num)
 SocketTCP SocketTCP::accept(InetAddr& addr, int flags)
 {
     detail::sockaddr temp;
+    detail::zeroMemory(&temp, sizeof(temp));
     int sockfd = detail::accept(sockfd_, temp, flags);
     addr = InetAddr(temp);
     return SocketTCP::attach(sockfd);
+}
+
+bool SocketTCP::connect(const InetAddr& addr)
+{
+    if (-1 == sockfd_ || !detail::connect(sockfd_, addr.getSockaddr()))
+        return false;
+
+    return true;
 }
 
 bool SocketTCP::close()
@@ -103,6 +132,26 @@ bool SocketTCP::close()
 
     sockfd_ = -1;
     return true;
+}
+
+bool SocketTCP::setTcpNoDelay(bool enable)
+{
+    return detail::setTcpNoDelay(sockfd_, enable);
+}
+
+bool SocketTCP::setReusePort(bool enable)
+{
+    return detail::setAddrReuse(sockfd_, enable);
+}
+
+bool SocketTCP::setReuseAddr(bool enable)
+{
+    return detail::setAddrReuse(sockfd_, enable);
+}
+
+bool SocketTCP::setKeepAlive(bool enable)
+{
+    return detail::setKeepAlive(sockfd_, enable);
 }
 
 }
