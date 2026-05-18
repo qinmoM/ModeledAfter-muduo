3c8a1ef2d9f7b435b5fed17ee258c55f764cd549
qinmoM_Linux <qinmo666@outlook.com>
2026-05-18 21:24:03 +0800

    test code : PacketBuffer

--- src/main.cpp
+++ src/main.cpp
@@ -4,8 +4,9 @@
 // #include "qinmo/net/SocketTCP.h"
 // #include "qinmo/base/StringConcat.h"
 // #include "qinmo/base/Logger.h"
-// #include "qinmo/net/TcpConnect.h"
-#include "qinmo/net/TcpListen.h"
+#include "qinmo/net/TcpConnect.h"
+// #include "qinmo/net/TcpListen.h"
+#include "qinmo/net/PacketBuffer.h"
 #include <iostream>
 
 // using qinmo::Timestamp;
@@ -73,29 +74,65 @@ int main()
     // std::cout << buf << std::endl;
     // connect.send("123456789", 9);
 
+    // using namespace qinmo::net;
+    // InetAddr serAddr;
+    // serAddr.setIP("192.168.87.90");
+    // serAddr.setPort(7129);
+    // TcpListen lsock = TcpListen::createRaw(serAddr);
+    //
+    // #define TEMP_EOE(isTrue, describe) if (!isTrue) { perror(describe); return -1; }
+    //
+    // TEMP_EOE(lsock.isValid(), "isValid");
+    // TEMP_EOE(lsock.setReuseAddr(true), "reuse address");
+    // TEMP_EOE(lsock.setReusePort(true), "reuse port");
+    // TEMP_EOE(lsock.bind(serAddr), "bind");
+    // TEMP_EOE(lsock.listen(), "listen");
+    // InetAddr cliAddr;
+    // TcpConnect cli = lsock.accept(cliAddr);
+    // TEMP_EOE(0 >= cli.send("ok\n", 3), "send");
+    // char buf[1024] = "";
+    // TEMP_EOE(0 >= cli.recv(buf, sizeof(buf)), "recv");
+    // buf[1023] = '\0';
+    // std::cout << buf << std::endl;
+    // InetAddr local = lsock.getLocalAddr();
+    // std::cout << local.getIP() << std::endl;
+    // std::cout << cli.getPeerAddr().getIP() << std::endl;
+
     using namespace qinmo::net;
+    #define TEMP_EOE(isTrue, describe) if (!isTrue) { std::cout << describe << std::endl; return -1; }
+
     InetAddr serAddr;
-    serAddr.setIP("192.168.87.90");
+    serAddr.setIP("192.168.87.212");
     serAddr.setPort(7129);
-    TcpListen lsock = TcpListen::createRaw(serAddr);
-    
-    #define TEMP_EOE(isTrue, describe) if (!isTrue) { perror(describe); return -1; }
-    
-    TEMP_EOE(lsock.isValid(), "isValid");
-    TEMP_EOE(lsock.setReuseAddr(true), "reuse address");
-    TEMP_EOE(lsock.setReusePort(true), "reuse port");
-    TEMP_EOE(lsock.bind(serAddr), "bind");
-    TEMP_EOE(lsock.listen(), "listen");
-    InetAddr cliAddr;
-    TcpConnect cli = lsock.accept(cliAddr);
-    TEMP_EOE(0 >= cli.send("ok\n", 3), "send");
-    char buf[1024] = "";
-    TEMP_EOE(0 >= cli.recv(buf, sizeof(buf)), "recv");
-    buf[1023] = '\0';
-    std::cout << buf << std::endl;
-    InetAddr local = lsock.getLocalAddr();
-    std::cout << local.getIP() << std::endl;
-    std::cout << cli.getPeerAddr().getIP() << std::endl;
+    TEMP_EOE(serAddr.isValid(), "server address error.");
+    TcpConnect connect = TcpConnect::connectRaw(serAddr);
+    TEMP_EOE(connect.isValid(), "connect invalid.");
+
+    PacketBuffer bufOut(1);
+    std::string s = "Hello, it's a connect to test PacketBuffer class. Reply something, please.\n";
+    bufOut.appendString(s);
+    TEMP_EOE(bufOut.headSet8(0, s.size()), "protocol header error");
+    ssize_t len = 0;
+    int savederror = 0;
+    len = bufOut.writeFd(connect.getfd(), savederror);
+    int8_t headerNumbet = 0;
+    std::cout << "len:" << len
+            << ", header:" << bufOut.headGet8(0, headerNumbet)
+            << ", size:" << (int)headerNumbet
+            << ", error code:" << savederror
+            << std::endl;
+
+    // PacketBuffer bufIn(0);
+    // len = bufIn.readFd(connect.getfd(), savederror, 12);
+    // bufIn.retrieveAll(s);
+    // std::cout << s << std::endl;
+
+    PacketBuffer bufIn(1);
+    len = bufIn.readFd(connect.getfd(), savederror, 12);
+    TEMP_EOE(bufIn.retrieve8(headerNumbet), "retrieve8");
+    TEMP_EOE(bufIn.headSet8(0, headerNumbet), "headSet8");
+    TEMP_EOE(bufIn.retrieveAll(s), "retrieveAll");
+    std::cout << "len:" << len << ", size:" << headerNumbet << ", text:" << s << std::endl;
 
     return 0;
 }
