048a512f21d30fac187d5ff92b44692111b589ae
qinmoM_Linux <qinmo666@outlook.com>
2026-05-24 21:22:55 +0800

    feat(net.h) : Add comments

--- include/qinmo/net.h
+++ include/qinmo/net.h
@@ -1,3 +1,15 @@
+/**
+ * @file <qinmo/net.h>
+ * @brief A Simple Net Library
+ * @details
+ *      This module covers business ReactorTCPServer & ReactorTCPConnect.
+ *      Low-level SocketTCP implementation.
+ *      And common non-network utilities including print, StringView, Logger ...
+ * 
+ * @version 0.0.1
+ * @author qinmoM
+ * @github https://github.com/qinmoM/qinmo_net
+ */
 #pragma once
 
 #include <qinmo/net/ReactorTcpServer.h>
