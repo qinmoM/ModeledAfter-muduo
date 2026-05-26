106319cd93526884e98f1d92f54cd7fe906d3934
qinmoM_Linux <qinmo666@outlook.com>
2026-05-26 20:15:48 +0800

    feat : Timer interface

---
+++ include/qinmo/net/Timer.h
@@ -0,0 +1,38 @@
+#pragma once
+
+#include "../base/Timestamp.h"
+#include <functional>
+
+namespace qinmo
+{
+/// @namespace qinmo::net
+namespace net
+{
+
+using TimerFunc = std::function<void()>;
+
+class Timer
+{
+public:
+    Timer(TimerFunc func, Timestamp when, double intervalSeconds);
+    ~Timer();
+
+public:
+    uint64_t getSequence() const;
+    Timestamp getTimestamp() const;
+    int64_t getInterval() const;
+
+    void run();
+    void reset();
+
+private:
+    static std::atomic<uint64_t> s_currSeq_;
+
+    TimerFunc func_;
+    qinmo::Timestamp time_;
+    uint64_t seq_;
+    int64_t interval_;
+
+};
+} // namespace net
+} // namespace qinmo
