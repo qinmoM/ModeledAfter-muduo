d5f77c8022d956c77642b0b22ea589a8d4ff76ad
qinmoM_Linux <qinmo666@outlook.com>
2026-05-27 13:28:30 +0800

    feat : Implement cancelEvent function

--- src/TimerManager.cpp
+++ src/TimerManager.cpp
@@ -13,6 +13,8 @@ TimerManager::TimerManager(EventLoop* loop)
     : loop_(loop)
     , fd_(qinmo::detail::timerfd_create(CLOCK_MONOTONIC, TFD_NONBLOCK | TFD_CLOEXEC))
     , channel_(loop_, fd_)
+    , currTimer_(0)
+    , cancelTimer_(0)
 {
     if (0 > fd_)
     {
@@ -53,7 +55,24 @@ TimerID TimerManager::addEvent(TimerFunc func, Timestamp when, double interval)
 
 void TimerManager::cancelEvent(TimerID id)
 {
-    ;
+    loop_->runInLoop(
+        [this, id]()
+        {
+            auto it = timers_.find(id.seq_);
+            if (it == timers_.end())
+                return;
+
+            uint64_t s = timersOrder_.begin()->second;
+            timersOrder_.erase(TimerEntry{ it->second->getTimestamp(), id.seq_ });
+            if (id.seq_ == currTimer_)
+                cancelTimer_ = id.seq_;
+            else
+                timers_.erase(it);
+
+            if (s != timersOrder_.begin()->second)
+                resetTimerfd(timersOrder_.begin()->first);
+        }
+    );
 }
 
 void TimerManager::handleRead(Timestamp time)
@@ -65,12 +84,43 @@ void TimerManager::handleRead(Timestamp time)
             QINMO_ERROR("TimerManager.handleRead: read bytes is less than 8 byte:", len, ".");
     }
 
-    ;
+    while (!timersOrder_.empty())
+    {
+        auto it = timersOrder_.begin();
+
+        if (it->first > time)
+            break;
+
+        ;
+    }
+
+    // for (const auto& it : timersOrder_)
+    // {
+    //     timers_.erase(cancelTimer_);
+    //     cancelTimer_ = 0;
+
+    //     if (time < it.first)
+    //         return;
+
+    //     auto timer = timers_.find(it.second);
+    //     if (timer == timers_.end())
+    //     {
+    //         QINMO_ERROR("Failed to find timer from timersOrder in timers. tfd=", fd_, ", sequence=", it.second);
+    //         return;
+    //     }
+
+    //     currTimer_ = it.second;
+    //     timer->second->run();
+    //     currTimer_ = 0;
+    // }
+
+    // timers_.erase(cancelTimer_);
+    // cancelTimer_ = 0;
 }
 
 void TimerManager::resetTimerfd(Timestamp when)
 {
-    int64_t t = (when - Timestamp::now().getMicroseconds()).getMicroseconds();
+    int64_t t = std::max(int64_t(1), (when - Timestamp::now().getMicroseconds()).getMicroseconds());
     struct itimerspec newVal;
     detail::zeroMemory(&newVal, sizeof(newVal));
     struct timespec ts;
